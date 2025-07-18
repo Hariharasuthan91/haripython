38c53338848205965ede263e4d7b7b4e78db1852
Hariharasuthan91 <hariharanexamcert@gmail.com>
2025-07-18 19:54:10 +0530

    Add files via upload
    ASSIGNMENT 1:
    Module 2: Basic Python Concepts
     
    Task 1: Perform Basic Mathematical Operations
    Problem Statement: Write a Python program that does the following:
    1.  Takes two numbers as input from the user.
    2.  Performs the basic mathematical operations on these two numbers:
    o	Addition
    o	Subtraction
    o	Multiplication
    o	Division
    3.  Displays the results of each operation on the screen.
     Expected Output:
    The output should include the result of each operation performed, for example:
     
    Task 2: Create a Personalized Greeting
    Problem Statement: Write a Python program that:
    1.  Takes a user's first name and last name as input.
    2.  Concatenates the first name and last name into a full name.
    3.  Prints a personalized greeting message using the full name.
    Expected Output:
    The program should output a greeting like:
     
     
    Submission Instructions:
    Create a GitHub repository and upload your Python scripts (.py files).
    Ensure the repository includes a README.md file that describes the functionality of your programs.
    Add both Task 1 and Task 2 scripts in the same repository.
    Submit the link to your GitHub repository once uploaded.
    
    Reference:
    Follow the Python course - Module 2: Basic Python Concepts for additional guidelines and examples. 
    Note: Please test your project thoroughly and check all the validations and error handling prior to ensure it works as expected before submission.
    You can always connect to the mentor using the chat support option for any doubts or queries!

---
+++ arithmatic task1.py
@@ -0,0 +1,11 @@
+import math
+a=int(input('Enter the first number: '))
+b=int(input('Enter the second number: '))
+addition=a+b
+subtract=a-b
+multiply=a*b
+divide=a/b
+print('Addition:', {addition})
+print('Subtraction:', {subtract})
+print('Multiplication:', {multiply})
+print('Division:', {divide})
